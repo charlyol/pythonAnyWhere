0e294aaecccadce629cfb60d4b13678485d8e326
charlyol <147928338+charlyol@users.noreply.github.com>
2024-05-30 12:56:54 +0200

    Automatic deployment - 2024-05-30 12:56:54

--- bottle_app.py
+++ bottle_app.py
@@ -1,6 +1,10 @@
 
 # A very simple Bottle Hello World app for you to get started with...
-from bottle import default_app, route, run, template
+from bottle import default_app, route, run, template, TEMPLATE_PATH
+import os
+
+path_directory = os.path.join(os.getcwd(), 'views')
+TEMPLATE_PATH.insert(0, path_directory)
 
 
 @route('/')

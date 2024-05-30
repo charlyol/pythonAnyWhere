b3c5b74923e034bf5fc2e5569532799ffc4efee8
charlyol <147928338+charlyol@users.noreply.github.com>
2024-05-30 11:28:00 +0200

    Déploiement automatique - 2024-05-30 11:28:00

--- deploy.py
+++ deploy.py
@@ -22,7 +22,7 @@ def deploy():
     # api_token = '6c662ac265f1f9a47fa07f027bc52313e809e636'
     #
     # # Endpoints de l'API PythonAnywhere
-    git_pull_url = f"https://www.pythonanywhere.com/api/v0/user/{username}/gitpull/"
+    # git_pull_url = f"https://www.pythonanywhere.com/api/v0/user/{username}/gitpull/"
     # reload_app_url = f"https://www.pythonanywhere.com/api/v0/user/{username}/webapps/CharlyOlinger.pythonanywhere.com/reload/"
     #
     # # En-têtes de requête avec l'authentification
@@ -37,15 +37,15 @@ def deploy():
     # reload_app_response = requests.post(reload_app_url, headers=headers)
     #
     # # Vérifier les réponses
-    if git_pull_response.status_code == 200:
-        print("Dépôt Git mis à jour avec succès")
-    else:
-        print(f"Échec de la mise à jour du dépôt Git : {git_pull_response.text}")
-
-    if reload_app_response.status_code == 200:
-        print("Application rechargée avec succès")
-    else:
-        print(f"Échec du rechargement de l'application : {reload_app_response.text}")
+    # if git_pull_response.status_code == 200:
+    #     print("Dépôt Git mis à jour avec succès")
+    # else:
+    #     print(f"Échec de la mise à jour du dépôt Git : {git_pull_response.text}")
+    #
+    # if reload_app_response.status_code == 200:
+    #     print("Application rechargée avec succès")
+    # else:
+    #     print(f"Échec du rechargement de l'application : {reload_app_response.text}")
 
 if __name__ == "__main__":
     deploy()

421f847a04bce997a4a7500b447127f4722eb8e5
charlyol <147928338+charlyol@users.noreply.github.com>
2024-05-30 10:56:04 +0200

    vlvkzd

--- bottle_app.py
+++ bottle_app.py
@@ -1,10 +1,14 @@
 
 # A very simple Bottle Hello World app for you to get started with...
-from bottle import default_app, route
+from bottle import default_app, route, run
+
 
 @route('/')
 def hello_world():
-    return 'Hello from Bottle!'
+    return 'Hello World ! This is the first page of the project.'
+
 
 application = default_app()
 
+if __name__ == '__main__':
+    run()
